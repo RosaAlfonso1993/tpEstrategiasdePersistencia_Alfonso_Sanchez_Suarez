db55dd60a1d254092e0fcd391a4e9440f151a895
leandro1994s <beto.unahur@gmail.com>
2020-11-05 15:47:29 -0500

    arreglado instituo_carrera

--- api/models/instituto_carrera.js
+++ api/models/instituto_carrera.js
@@ -18,18 +18,18 @@ module.exports = (sequelize, DataTypes) => {
     },{});
     instituto_carrera.associate = function (models) {
       // GUARDA!
-      /*
+      
         instituto_carrera.hasOne(models.carrera
             , {
                 foreignKey: 'id',
                 sourceKey: 'id_carrera_fk'
             });
-            alumno_materia.hasOne(models.instituto
+        instituto_carrera.hasOne(models.instituto
             , {
                 foreignKey: 'id',
                sourceKey: 'id_instituto_fk'
             });
-        */
+        
      };
 
     return instituto_carrera;
